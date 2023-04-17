725d4c866e69b30da1f0e093ce7d51436c7cd5e9
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-04-17 10:54:17 -0300

    Paginas Iniciais

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD-YjYEU8jRylYS5IIsX5298WqvvMoTq7k',
+  authDomain: 'miniblog-9c7ae.firebaseapp.com',
+  projectId: 'miniblog-9c7ae',
+  storageBucket: 'miniblog-9c7ae.appspot.com',
+  messagingSenderId: '775854497129',
+  appId: '1:775854497129:web:47805ee0ded67d4569f1f8',
+  measurementId: 'G-DTDJZV511Q',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db, analytics };
---
+++ src/pages/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//CSS
+import styles from './About.module.css';
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+export default About;
