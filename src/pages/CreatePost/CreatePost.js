a0009d813d35cb07d2f25e2bdf248f86b5dd6e08
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-04-18 11:30:15 -0300

    Novo Post

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -4,6 +4,7 @@ import styles from './CreatePost.module.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthValue } from '../../context/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocument';
 
 const CreatePost = () => {
   const [title, setTitle] = useState('');
@@ -12,8 +13,41 @@ const CreatePost = () => {
   const [tags, setTags] = useState('');
   const [formError, setFormError] = useState('');
 
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument('posts');
+
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError('');
+
+    //validar URL da imagem
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError('A imagem precisa ser uma URL');
+    }
+    //criar array de tags
+    const tagsArray = tags.split(',').map((tag) => tag.trim().toLowerCase());
+    //checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError('Por favor, preencha todos os campos!');
+    }
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    //redirect to home page
+    navigate('/');
   };
   return (
     <div className={styles.create_post}>
@@ -63,7 +97,14 @@ const CreatePost = () => {
             value={tags}
           />
         </label>
-        <button className="btn">Postar</button>
+        {!response.loading && <button className="btn">Postar</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Carregando...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
