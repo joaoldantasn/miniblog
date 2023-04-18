2b2a89b7fdc6bb5e3a5cd5bf2af6923565a16550
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-04-17 23:28:14 -0300

    Bloqueando pags que precisam de autenticacao e criando pagina sobre

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 //CSS
 import styles from './About.module.css';
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini<span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com react no front-end e Firebase
+        no back-end.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
+    </div>
+  );
 };
 
 export default About;
